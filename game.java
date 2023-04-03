007660de02a1fbd765fd7a9b66cc1ff05ad79c27
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-03 23:06:00 +0530

    ADDED:code to get random number

--- game.java
+++ game.java
@@ -1,26 +1,26 @@
 package com.snakeladder;
 
 public class game {
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         int player1 = 0;
-        int ran1 = (int) ((Math.random() * 10) % 6 + 1);
+      int ran1 = (int) ((Math.random() * 10) % 6 + 1);
         System.out.println(ran1);
-        int ran2 = (int) ((Math.random() * 10) % 2 + 1);
-        System.out.println(ran2);
-
-        switch (ran2) {
-            case 1:
-                System.out.println("no play");
-                break;
-            case 2:
-                System.out.println("player move ahead of " + ran1);
-                break;
-            case 3:
-                System.out.println("player move behind of" + ran1);
-                break;
-            default:
-                System.out.println("no case");
-                break;
-        }
-    }
+//        int ran2 = (int) ((Math.random() * 10) % 2 + 1);
+//        System.out.println(ran2);
+//
+//        switch (ran2) {
+//            case 1:
+//                System.out.println("No Play");
+//                break;
+//            case 2:
+//                System.out.println("Ladder for " + ran1);
+//                break;
+//            case 3:
+//                System.out.println("Snake for " + ran1);
+//                break;
+//            default:
+//                System.out.println("no case");
+//                break;
+//        }
+   }
 }

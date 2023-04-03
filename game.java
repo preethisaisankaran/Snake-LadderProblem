6d4729cbe340d1f55ffd1ede29f6f5f3411bf1dd
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-03 22:52:25 +0530

    ADDED:Player check option

--- game.java
+++ game.java
@@ -1,9 +1,26 @@
 package com.snakeladder;
 
 public class game {
-    public static void main(String[] args){
-        int player1=0;
-        int ran1 = (int) Math.random()*6+1;
+    public static void main(String[] args) {
+        int player1 = 0;
+        int ran1 = (int) ((Math.random() * 10) % 6 + 1);
         System.out.println(ran1);
-  }
+        int ran2 = (int) ((Math.random() * 10) % 2 + 1);
+        System.out.println(ran2);
+
+        switch (ran2) {
+            case 1:
+                System.out.println("no play");
+                break;
+            case 2:
+                System.out.println("player move ahead of " + ran1);
+                break;
+            case 3:
+                System.out.println("player move behind of" + ran1);
+                break;
+            default:
+                System.out.println("no case");
+                break;
+        }
+    }
 }

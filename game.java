8c6a22f376fdff9b3fc750a0bd0cfe531c948b5a
preethisaisankaran <preethisankaran11@gmail.com>
2023-04-03 22:29:51 +0530

    ADDED:Get random number from 1-6

--- game.java
+++ game.java
@@ -3,7 +3,7 @@ package com.snakeladder;
 public class game {
     public static void main(String[] args){
         int player1=0;
-
-
-    }
+        int ran1 = (int) Math.random()*6+1;
+        System.out.println(ran1);
+  }
 }
